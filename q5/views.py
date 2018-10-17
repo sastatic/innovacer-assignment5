ba4db6c3bfe978f5a46c99c1844a3ff7a3b7af49
sastatic <sarwaralisiddiqui@gmail.com>
2018-10-17 19:56:35 +0530

    requirements added

--- q5/views.py
+++ q5/views.py
@@ -4,13 +4,7 @@ from django.db.models import Q
 
 from .models import series, user, subscription
 from .forms import SubscriptionForm
-# from .imdb_page import getLinks
-
-import bs4 as bs
-import urllib.request
-from urllib.request import Request, urlopen
-
-
+from .imdb_page import getLinks
 def index(request):
     if request.method == 'POST':
         form = SubscriptionForm(request.POST)
@@ -21,8 +15,8 @@ def index(request):
             imdb_links = []
             for series in serieses:
                 link = getLinks(series)
-            #     imdb_links.append(link)
-            #     print ("Imdb Page for "+series+" is "+link)
+                imdb_links.append(link)
+                print ("Imdb Page for "+series+" is "+link)
             return render(request, 'index.html', {'form': form})
     else:
         form = SubscriptionForm()
@@ -33,24 +27,3 @@ def subscribe(request):
     if request.method == "POST":
         pass
     return render(request, 'subscribed.html')
-
-def getLinks(search):
-    search = search.strip()
-    search = search.replace(" ", "+")
-    url = 'https://www.imdb.com/find?ref_=nv_sr_fn&q='+search+'&s=tt'
-
-    req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
-    source = urlopen(req).read()
-
-    soup = bs.BeautifulSoup(source, 'lxml')
-    Mdiv = soup.find('table', {'class':'findList'})
-    table = Mdiv.find_all('td', {'class':'result_text'})
-
-    tag = 'tv series'
-
-    for row in table:
-        if tag.upper() in row.get_text().strip().upper():
-            link = 'https://www.imdb.com'+row.find('a').get('href')
-            break
-            
-    return link
